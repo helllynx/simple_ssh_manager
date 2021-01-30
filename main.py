4544b80fa7c6872829e78e987d4fd0c75000227b
Yanis <white.night.447@gmail.com>
2021-01-30 15:18:18 +0300

    Add terminal screen clean after each command.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from app import locale_manager
 from app.command import Command, run_command, print_all_commands, get_func_by_id
+from app.utils import clear_terminal
 
 if __name__ == '__main__':
+    clear_terminal()
     print(locale_manager.get_localized_string('greet'))
 
     while True:
         try:
             print_all_commands()
             command_id = int(input('> '))
+            clear_terminal()
             assert command_id >= 0 and get_func_by_id(command_id)
             if run_command(command_id) == Command.EXIT:
                 break
